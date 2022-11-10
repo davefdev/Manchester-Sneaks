0d6f32b3d7445286b408db70c75578871b4a68d5
Davef <76170782+davefdev@users.noreply.github.com>
2022-11-10 20:56:12 +0000

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React from "react";
+import css from "./Header.module.css";
 
 const Header = () => {
   return (
-    <div>Header
-  
-  
-    </div>
-  )
-}
+    <div className={css.container}>
+      <div className={css.logo}>Logo</div>
 
-export default Header
+      <div className={css.right}>
+        <div className={css.menu}>
+
+        </div>
+
+        <input type="text" className={css.search} />
+
+        <span>PNG icon of cart</span>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
