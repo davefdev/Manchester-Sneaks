3da55338b7469d5b5bea1251d06c30fec82030e3
Davef <76170782+davefdev@users.noreply.github.com>
2022-11-21 14:06:03 +0000

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
           <ul className={css.menu}>
             <li>Collections</li>
             <li>Brands</li>
-            <li>New</li>
+            <li>New Product</li>
             <li>Sales</li>
             <li>Eng</li>
           </ul>
